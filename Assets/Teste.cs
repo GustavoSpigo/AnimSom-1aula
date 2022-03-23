fc8b071f16b5c54bc4f754b664ee4ba01b40321f
Gustavo Gomes <gustavo@spigo.net>
2022-03-22 21:30:19 -0300

    Novo comentário

--- Assets/Teste.cs
+++ Assets/Teste.cs
@@ -7,7 +7,7 @@ public class Teste : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Esse é um comentrario
     }
 
     // Update is called once per frame
